91fe9b651147cb4baacbbe09b0a10993f32abc70
spankratov <spankratov@users.noreply.github.com>
2018-06-06 21:30:01 +0300

    Update middleware.py

--- lokalise/middleware.py
+++ lokalise/middleware.py
@@ -12,7 +12,7 @@ class ReloadTranslationsMiddleware(object):
         locale_mtime = getmtime(get_locale_path())
         try:
             thread_local = threading.local
-            if locale_mtime > thread_local.locale_mtime or not hasattr(thread_locale, 'locale_mtime'):
+            if not hasattr(thread_locale, 'locale_mtime') or locale_mtime > thread_local.locale_mtime:
                 thread_local.locale_mtime = locale_mtime
                 reset_translations()
         except:

8f3a8a64367ea3fa6e3223fa633652e0aa9efe9b
spankratov <spankratov@users.noreply.github.com>
2018-12-25 02:00:59 +0300

    Update middleware.py

--- lokalise/middleware.py
+++ lokalise/middleware.py
@@ -1,5 +1,6 @@
 import threading
-from os.path import getmtime
+import os, errno
+from os.path import getmtime, exists as path_exists
 
 from django.utils import translation
 from django.utils.autoreload import reset_translations
@@ -12,6 +13,14 @@ from . import get_locale_path
 class ReloadTranslationsMiddleware(MiddlewareMixin):
     @staticmethod
     def process_request(request):
+        locale_path = get_locale_path()
+        if not path_exists(locale_path):
+            try:
+                os.makedirs(locale_path)
+            except OSError as e:
+                # https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory-in-python
+                if e.errno != errno.EEXIST:
+                    raise
         locale_mtime = getmtime(get_locale_path())
         try:
             thread_local = threading.local()
